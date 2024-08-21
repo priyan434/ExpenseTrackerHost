66847ea7d25a4549fa64b9a9b38523530e7a0a13
priyan434 <priyanr494@gmail.com>
2024-08-21 13:54:50 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const Sequelize = require("sequelize");
+require('dotenv').config()
+console.log(process.env.DATABASE_HOST);
+
+const sequelize = new Sequelize(
+ process.env.MYSQL_ADDON_DB,
+ process.env.MYSQL_ADDON_USER,
+ process.env.MYSQL_ADDON_PASSWORD,
+  {
+    host: process.env.MYSQL_ADDON_HOST,
+    port: process.env.MYSQL_ADDON_PORT,
+    dialect: process.env.DATABASE_DIALECT
+  },
+ 
+);
+
+
+module.exports=sequelize
